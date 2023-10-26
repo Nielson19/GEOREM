32dc9594a09a5d07f7728a4d7f6ce0ec2e614820
jcasa28 <jsuscasasanta@gmail.com>
2023-10-25 22:55:18 -0400

    bottom menu button for map expansion

--- app/src/main/java/com/example/neil/MainActivity.kt
+++ app/src/main/java/com/example/neil/MainActivity.kt
@@ -1,15 +1,18 @@
 package com.example.neil
-
+import androidx.constraintlayout.widget.ConstraintLayout.LayoutParams
 import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.animation.Animation
 import android.view.animation.AnimationUtils
 import android.view.animation.LayoutAnimationController
 import android.widget.Button
+import android.widget.FrameLayout
+import android.animation.ValueAnimator
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.neil.databinding.ActivityMainBinding
@@ -34,6 +37,18 @@ class MainActivity : AppCompatActivity(), OnItemCLickReminder {
     private val reminderModelList = ArrayList<ReminderModel>()
 
     private lateinit var bottomMenuAddRemItem: BottomNavigationView
+
+    //Variables For expanding the map
+    private lateinit var mapWindow: FrameLayout
+    private lateinit var bottomWindow: ConstraintLayout
+
+
+    private var originalHeightMap: Int = 0
+    private var originalWidth: Int = 0
+    private var originalHeight2: Int = 0
+    //variable used as a flag
+    private var isExpanded = false
+
     //FUNCTIONS
 
     //Bottom menu selection space
@@ -55,6 +70,14 @@ class MainActivity : AppCompatActivity(), OnItemCLickReminder {
         val recyclerView: RecyclerView = findViewById(R.id.reminderList)
         setReminderModel()
 
+        // Initialize the FrameLayout and Button
+        mapWindow = findViewById(R.id.mapWindow)
+        bottomWindow=findViewById(R.id.BottomWindow)
+
+        originalHeightMap = mapWindow.height
+        originalWidth = mapWindow.width
+        originalHeight2 = bottomWindow.height
+
 
         // bottom menu buttons executable
 
@@ -66,6 +89,8 @@ class MainActivity : AppCompatActivity(), OnItemCLickReminder {
                 }
                 R.id.mapIcon -> {
                     // map icon executable
+                    toggleFrameLayout()
+
                 }
                 R.id.settingsIcon ->{
 
@@ -88,7 +113,116 @@ class MainActivity : AppCompatActivity(), OnItemCLickReminder {
 
     }
 
+    //    expand map function
+    @SuppressLint("Recycle")
+    private fun expandFrameLayout() {
+        // Define the original height and width for the mapWindow
+        //The variables are located in the "dimentions.xml" file (the ones after the R.dimen)
+        val originalHeightMap = resources.getDimensionPixelSize(R.dimen.expanded_height)
+        val originalWidthMap = resources.getDimensionPixelSize(R.dimen.expanded_width)
+        val originalHeightBottom = resources.getDimensionPixelSize(R.dimen.expanded_height)
 
+        // Create ValueAnimators to animate the height and width of the mapWindow
+        val valueAnimatorHeightMap = ValueAnimator.ofInt(mapWindow.height, originalHeightMap)
+        val valueAnimatorWidthMap = ValueAnimator.ofInt(mapWindow.width, originalWidthMap)
+        val valueAnimatorHeightBottom = ValueAnimator.ofInt(bottomWindow.height, originalHeightBottom)
+
+        // Set animation duration in milliseconds
+        valueAnimatorHeightMap.duration = 500
+        valueAnimatorWidthMap.duration = 500
+        valueAnimatorHeightBottom.duration =500
+
+        // Add update listeners to animate the layout params
+        // These are the animations in the increasing frame
+        // Animate the height of the mapWindow
+        valueAnimatorHeightMap.addUpdateListener { animator ->
+            val layoutParams = mapWindow.layoutParams as LayoutParams
+            layoutParams.height = animator.animatedValue as Int
+            mapWindow.layoutParams = layoutParams
+        }
+        // Animate the width of the mapWindow
+        valueAnimatorWidthMap.addUpdateListener { animator ->
+            val layoutParams = mapWindow.layoutParams as LayoutParams
+            layoutParams.width = animator.animatedValue as Int
+            mapWindow.layoutParams = layoutParams
+        }
+
+
+
+        // Animate the height of the bottomWindow
+        valueAnimatorHeightBottom.addUpdateListener { animator ->
+            val layoutParams = bottomWindow.layoutParams as LayoutParams
+            layoutParams.height = animator.animatedValue as Int
+            bottomWindow.layoutParams = layoutParams
+        }
+
+        // Start the animations
+        valueAnimatorHeightMap.start()
+        valueAnimatorWidthMap.start()
+        valueAnimatorHeightBottom.start()
+
+
+    }
+
+
+
+
+    // function to minimize the map
+    private fun minimizeFrameLayout() {
+        // Define the original height and width for the mapWindow and the bottomWindow
+        //The variables are located in the "dimentions.xml" file (the ones after the R.dimen)
+        val originalHeightMap = resources.getDimensionPixelSize(R.dimen.original_map_height)
+        val originalWidthMap = resources.getDimensionPixelSize(R.dimen.original_map_width)
+        val originalHeightBottom = resources.getDimensionPixelSize(R.dimen.original_bottom_height)
+
+        // Create ValueAnimators to animate the height and width of the mapWindow and the bottomWindow
+        val valueAnimatorHeightMap = ValueAnimator.ofInt(mapWindow.height, originalHeightMap)
+        val valueAnimatorWidthMap = ValueAnimator.ofInt(mapWindow.width, originalWidthMap)
+        val valueAnimatorHeightBottom = ValueAnimator.ofInt(bottomWindow.height, originalHeightBottom)
+
+        // Set the same duration for animations
+
+        valueAnimatorHeightMap.duration = 500
+        valueAnimatorWidthMap.duration =500
+        valueAnimatorHeightBottom.duration = 500
+
+        // Animate the height of the mapWindow
+        valueAnimatorHeightMap.addUpdateListener { animator ->
+            val layoutParams = mapWindow.layoutParams as LayoutParams
+            layoutParams.height = animator.animatedValue as Int
+            mapWindow.layoutParams = layoutParams
+        }
+
+        // Animate the width of the mapWindow
+        valueAnimatorWidthMap.addUpdateListener { animator ->
+            val layoutParams = mapWindow.layoutParams as LayoutParams
+            layoutParams.width = animator.animatedValue as Int
+            mapWindow.layoutParams = layoutParams
+        }
+        // Animate the height of the bottomWindow
+        valueAnimatorHeightBottom.addUpdateListener { animator ->
+            val layoutParams = bottomWindow.layoutParams as LayoutParams
+            layoutParams.height = animator.animatedValue as Int
+            bottomWindow.layoutParams = layoutParams
+        }
+
+        // Start the animations
+        valueAnimatorHeightMap.start()
+        valueAnimatorWidthMap.start()
+        valueAnimatorHeightBottom.start()
+    }
+
+
+    // function to make the button work in both functions
+    private fun toggleFrameLayout() {
+        if (isExpanded) {
+            minimizeFrameLayout()
+        } else {
+            expandFrameLayout()
+        }
+        // Toggle the isExpanded flag AFTER the animation is complete
+        isExpanded = !isExpanded
+    }
 
 
     // Add reminder window pop up function
